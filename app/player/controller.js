5c72b68724d79b921608d38b5c5a1836f6c476cc
maulllanamm <maulllanamuhammad@gmail.com>
2022-04-12 14:49:39 +0700

    edit controller detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -27,12 +27,13 @@ module.exports = {
         .populate("nominals")
         .populate("user", "_id name phoneNumber");
 
+      const payment = await Payment.find().populate("banks");
       if (!voucher) {
         return res
           .status(404)
           .json({ message: "voucher game tidak di temukan" });
       }
-      res.status(200).json({ data: voucher });
+      res.status(200).json({ data: voucher, payment });
     } catch (error) {
       res
         .status(500)
